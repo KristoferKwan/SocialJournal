2ea33996647ed0856db1c00e9ea07c28e211e6b1
Covenant <cfaluyi@gmail.com>
2020-11-07 18:44:24 -0500

    Gave EntryComponenet props

--- socialjournal/src/EntryComponent.js
+++ socialjournal/src/EntryComponent.js
@@ -20,17 +20,17 @@ const useStyles = makeStyles((theme) => ({
   }));
 
   
-export const EntryComponent  = () => {
+export const EntryComponent  = ({name, date, message}) => {
     const classes = useStyles();
     return (
     <Paper className={classes.paper}>
         <Grid container wrap= "nowrap" spacing = {5}>
           <Grid container item direction= 'column' spacing = {3} flexGrow={1} xs = {3} wrap= "nowrap">
             <Grid item zeroMinWidth style={{ wordWrap: "break-word" }}>
-              <Typography> Covenant Faluyi </Typography>
+              <Typography> {name} </Typography>
             </Grid>
             <Grid item>
-              <Typography>Nov 8, 2020</Typography>
+              <Typography>{date}</Typography>
             </Grid>
           </Grid>
           <Grid item flexGrow= {2} xs = {9}>
@@ -40,7 +40,7 @@ export const EntryComponent  = () => {
               line={3}
               element="span"
               truncateText="…"
-              text="What the **** did you just fucking say about me, you little *****? I’ll have you know I graduated top of my class in the Navy Seals, and I’ve been involved in numerous secret raids on Al-Quaeda, and I have over 300 confirmed kills. I am trained in gorilla warfare and I’m the top sniper in the entire US armed forces. You are nothing to me but just another target. I will wipe you the fuck out with precision the likes of which has never been seen before on this Earth, mark my fucking words. You think you can get away with saying that shit to me over the Internet? Think again, fucker. As we speak I am contacting my secret network of spies across the USA and your IP is being traced right now so you better prepare for the storm, maggot. The storm that wipes out the pathetic little thing you call your life. You’re fucking dead, kid. I can be anywhere, anytime, and I can kill you in over seven hundred ways, and that’s just with my bare hands. Not only am I extensively trained in unarmed combat, but I have access to the entire arsenal of the United States Marine Corps and I will use it to its full extent to wipe your miserable ass off the face of the continent, you little shit. If only you could have known what unholy retribution your little “clever” comment was about to bring down upon you, maybe you would have held your fucking tongue. But you couldn’t, you didn’t, and now you’re paying the price, you goddamn idiot. I will shit fury all over you and you will drown in it. You’re fucking dead, kiddo."
+              text= {message} 
             />
           </Grid>
         </Grid>
--- socialjournal/src/TimeLine.js
+++ socialjournal/src/TimeLine.js
@@ -1,27 +1,7 @@
 import {Typography} from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
 import {EntryComponent} from './EntryComponent';
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    width: '100%',
-    height: '100%'
-  },
-  paper: {
-    padding: theme.spacing(2),
-    margin: 'auto',
-    maxWidth: 500,
-  },
-  image: {
-    width: 128,
-    height: 128,
-  },
-}));
-
-
 function TimeLine() {
-  const classes = useStyles();
-
   return (
     <div>
     <Typography variant="h4"> 
@@ -32,7 +12,7 @@ function TimeLine() {
       Here are your most recent entries:
     </Typography>
 
-    <EntryComponent/>
+    <EntryComponent name = "Covenant Faluyi" date = {new Date().toDateString()} message=" Navy seal copy pasta"/>
 
     </div>
   );
