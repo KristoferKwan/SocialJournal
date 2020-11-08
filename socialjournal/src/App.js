98d9069c2df180dcf1144c5d38e4d0e454e2e297
KristoferKwan <kristofer.kwan@gmail.com>
2020-11-08 02:17:10 -0500

    MERGE: some generic containers and styling

--- socialjournal/src/App.js
+++ socialjournal/src/App.js
@@ -1,11 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import Timeline from './components/timeline/TimeLine';
 import Questions from './components/questions/Question';
-import Reminder from './components/reminder/Reminder'
-import Button from '@material-ui/core/Button';
-
+import Reminder from './components/reminder/Reminder';
+import {SideMenu} from './components/drawer/Drawer';
+import './style/Content.css';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
-
+import Container from '@material-ui/core/Container';
 
 
 function App() {
@@ -20,13 +20,16 @@ function App() {
 
     return (
       <Router>
-        <div className="App">
+        <SideMenu/>
+        <Container fixed>
+        <div className="App content">
           <Switch>
             <Route path="/" exact component={() => <Timeline entries={entries}/>} />
             <Route path="/questions" exact component={() => <Questions entries={entries} setEntries={setEntries} contacts={contacts}/>} />
             <Route path="/reminders" exact component={Reminder} />
           </Switch>
         </div>
+        </Container>
       </Router>
   );
 }
